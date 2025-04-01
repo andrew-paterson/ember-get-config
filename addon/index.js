0afcc63ca8f368b6d61685a9f6f0e200347667e3
Andrew Paterson <andrew.paterson9@gmail.com>
2025-04-01 15:03:22 +0200

    Test 3

--- addon/index.js
+++ addon/index.js
@@ -1,7 +1,27 @@
 /* global require */
 import { getOwnConfig } from '@embroider/macros';
-import loadConfigFromMeta from './config-meta-loader.js';
 
 let configModulePath = `${getOwnConfig().modulePrefix}/config/environment`;
 
 export default loadConfigFromMeta(configModulePath);
+
+function loadConfigFromMeta(prefix) {
+  var _a;
+
+  let metaName = `${prefix}/config/environment`;
+
+  try {
+    let rawConfig =
+      (_a = document
+        .querySelector(`meta[name="${metaName}"]`)
+        .getAttribute('content')) !== null && _a !== void 0
+        ? _a
+        : '{}';
+
+    let config = JSON.parse(decodeURIComponent(rawConfig));
+
+    return config;
+  } catch (err) {
+    return `Could not read config from meta tag with name "${metaName}".`;
+  }
+}
